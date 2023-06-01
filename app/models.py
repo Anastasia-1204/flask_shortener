bb5a7f72310fe8f07461f9fd0407867d25d29589
Анастасия <kotylkoas@mail.ru>
2023-06-01 17:58:48 +0500

    add files

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from . import db
+import datetime
+import random
+import string
+
+SHORT_LEN = 6
+
+
+def get_short():
+    while True:
+        short = ''.join(random.choices(string.ascii_letters + string.ascii_letters, k=SHORT_LEN))
+        if URLmodel.query.filter(URLmodel.short == short):
+            continue
+        return short
+
+
+class URLmodel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(255), unique=True)
+    short = db.Column(db.String(SHORT_LEN), unique=True)
+    visits = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
